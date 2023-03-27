652a566c87c91ec47cf04e053e368eca89c0d8be
Mabel <chrisbarbz17@gmail.com>
2023-03-27 09:37:20 +0000

    0-reset_to_98 task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * reset_to_98 - update the value of an integer using pointers
+ * @n: int value to be updated
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
